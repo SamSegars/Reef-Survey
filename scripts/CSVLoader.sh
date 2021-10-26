caaa417184d58cd196df470c9b8fd73bd0c17952
Sam Segars <sam.segars@yahoo.com>
2021-10-26 14:37:11 -0400

    Removed duplicates from Fish.sql and modified CSVloader to work with changes.

--- scripts/CSVLoader.sh
+++ scripts/CSVLoader.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#cat Fishbutspecial.sql | mysql -u admin -h mysql.ciazfzbygfyt.us-east-1.rds.amazonaws.com -P 3306 -ppassword
+cat Fish.sql | mysql -u admin -h mysql.ciazfzbygfyt.us-east-1.rds.amazonaws.com -P 3306 -ppassword
 cat konbert-export-3f88d9987b3b4.sql | mysql -u admin -h mysql.ciazfzbygfyt.us-east-1.rds.amazonaws.com -P 3306 -ppassword

49886bf37499c970834bde60b09bc400ba0971f1
Garry Peffer <gpeffer98@gmail.com>
2021-10-27 09:06:38 -0400

    Deleted a couple of unneccesary files.

--- scripts/ConvertDataToCSVFile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Convert data to CSV file
-
-#Convert CSV file to SQL Insert statement that corresponds to schema
-
-echo "Enter file path (minus the format for text file)."
-read {file}.txt
-sed 's/ \+/,/g' ""${file}.txt"" > ""{$file}"".csv

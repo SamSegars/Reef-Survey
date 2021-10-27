9a389c90e2593b41dbffd38eb24b1be3dafe2eba
Sam Segars <sam.segars@yahoo.com>
2021-10-27 09:41:22 -0400

    Fixed an error where unique .sql was not unique

--- scripts/Conversion/ConvertCSVToSql.sh
+++ scripts/Conversion/ConvertCSVToSql.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 awk -F',' '{ printf("INSERT INTO location (Region,Subregion,StudyArea, Latitude, Longitude, management) VALUES(\"'%s'\",\"'%s'\",\"'%s'\",'%f','%f',\"'%s'\");\n ", $1, $2, $3, $8, $9, $10); }' 1.csv > location.sql
-sort -u location.sql > locations.sql
+sed 1d location.sql | sort -u  > locations.sql
 sed -i '1s/^/use\ reef-survey;\n/' locations.sql
 awk -F',' '{ printf("Insert into fish (family, scientificname, commonname, trophic) values(\"'%s'\",\"'%s'\", \"'%s'\", \" '%s' \");\n ", $12, $13, $14,$15); }' 1.csv > fish.sql
-sort -u fish.sql > fishs.sql
+sed 1d fish.sql |sort -u > fishs.sql
 sed -i '1s/^/use\ reef-survey;\n/' fishs.sql
 awk -F',' '{ printf("Insert into survey (batchcode, surveyindex, surveydate, structuretype) values(\"'%s'\",\"'%s'\",\(select str_to_date\(\"'%s'\",\%m \,\%D\,\%Y \)\), \"'%s'\");\n ", $5,$6 ,$7,$11); }' 1.csv > survey.sql
-sort -u survey.sql > surveys.sql
+sed 1d survey.sql | sort -u > surveys.sql
 sed -i '1s/^/use\ reef-survey;\n/' surveys.sql
 sed -i 's/\//,/g' surveys.sql
 sed -i 's/%D/%d/g' surveys.sql
